50fd8527da004467e59bf2887acd8b0f1998ad98
AkalUstat <akalustat.singh@gmail.com>
2020-12-14 09:09:59 -0800

    [feat]: install scripts wihtout cloning

--- install_scripts/main.sh
+++ install_scripts/main.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
-
-./scripts/install_brew.sh
-./scripts/ohmyzsh.sh
-./scripts/yadm.sh
-./scripts/asdf.sh
-./scripts/node_globals.sh
-./scripts/neovim.sh
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/install_brew.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/ohmyzsh.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/yadm.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/asdf.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/node_globals.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/neovim.sh)
