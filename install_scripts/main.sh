8d907e3e3a73a8f268b89aa15d7e67cc2282b11e
AkalUstat <akalustat.singh@gmail.com>
2021-01-11 07:58:08 -0800

    change script urls

--- install_scripts/main.sh
+++ install_scripts/main.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
-bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/install_brew.sh)
-bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/ohmyzsh.sh)
-bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/yadm.sh)
-bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/asdf.sh)
-bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/node_globals.sh)
-bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/main/install_scripts/scripts/neovim.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/old-mac/install_scripts/scripts/install_brew.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/old-mac/install_scripts/scripts/yadm.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/old-mac/install_scripts/scripts/asdf.sh)
+bash <(curl -s https://raw.githubusercontent.com/AkalUstat/dotfiles/old-mac/install_scripts/scripts/node_globals.sh)
+
